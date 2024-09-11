3c0e7a1c10dab8c0fbfdae0cef9a5d5c02708b15
User <sarnavskiy1997@mail.ru>
2024-09-11 22:12:22 +0300

    feat:222

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,49 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int[][][] multiDimensionalArray = new int[2][2][2];
+
+        Random random = new Random();
+
+        for (int i = 0; i < multiDimensionalArray.length; i++) {
+            for (int j = 0; j < multiDimensionalArray[i].length; j++) {
+                for (int k = 0; k < multiDimensionalArray[i][j].length; k++) {
+                    int num = random.nextInt(-150, 150);
+                    multiDimensionalArray[i][j][k] = num;
+                }
+            }
         }
+
+        for (int i = 0; i < multiDimensionalArray.length; i++) {
+            for (int j = 0; j < multiDimensionalArray[i].length; j++) {
+                System.out.println("Значения в ваших массивах: " + Arrays.toString(multiDimensionalArray[i][j]));
+            }
+            System.out.println();
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите целое число на которое необходимо увеличить каждый элемент массива: ");
+        int numIncrease = scanner.nextInt();
+
+        for (int i = 0; i < multiDimensionalArray.length; i++) {
+            for (int j = 0; j < multiDimensionalArray[i].length; j++) {
+                for (int k = 0; k < multiDimensionalArray[i][j].length; k++) {
+                    multiDimensionalArray[i][j][k] += numIncrease;
+                }
+            }
+        }
+
+
+        for (int i = 0; i < multiDimensionalArray.length; i++) {
+            for (int j = 0; j < multiDimensionalArray[i].length; j++) {
+                System.out.println("Значения в массивах после увеличения: " + Arrays.toString(multiDimensionalArray[i][j]));
+            }
+            System.out.println();
+        }
+        scanner.close();
     }
 }
